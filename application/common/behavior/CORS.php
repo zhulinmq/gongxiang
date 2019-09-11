d766af2578edefc5491536ac03d75274791e60f2
zhulin <zhulinmq@163.com>
2019-09-11 08:39:12 +0800

    commit

--- application/common/behavior/CORS.php
+++ application/common/behavior/CORS.php
@@ -13,8 +13,8 @@ class CORS
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET');
-        if(request()->isOptions()){
-            exit();
-        }
+//        if(request()->isOptions()){
+//            exit();
+//        }
     }
 }

4c83de2a7d8789b05e2cd5657076fce635092455
zhulin <zhulinmq@163.com>
2019-09-16 16:16:09 +0800

    commit

---
+++ application/common/model/WithdrawRecord.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhulin
+ * Date: 2019-09-16
+ * Time: 10:17
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class WithdrawRecord extends Model
+{
+
+// 开启自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = 'updatetime';
+
+}
